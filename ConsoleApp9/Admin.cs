b7c849bed337d5c109576d8330e36b397871010f
Fidan1243 <42434617+Fidan1243@users.noreply.github.com>
2021-05-07 15:24:06 +0400

    Update Admin.cs

--- ConsoleApp9/Admin.cs
+++ ConsoleApp9/Admin.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net;
-using System.Net.Mail;
+
 
 namespace Admin
 {
@@ -20,25 +19,7 @@ namespace Admin
         public int NotificationCount { get; private set; } = 0;
         public static int myId = 0;
 
-        public void SendMail(string mails, Post.Post[] posts, int idp, User.User user, string vl)
-        {
-            Random r = new Random();
-            using (MailMessage mail = new MailMessage())
-            {
-                mail.From = new MailAddress(mails);
-                mail.To.Add(Email);
-                mail.Subject = $"You have 1 notification!";
-                mail.Body = $"<h1>{user.Name} {vl} your post. The post's content: {posts[idp].Content}</h1>";
-                mail.IsBodyHtml = true;
-                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
-                {
-                    smtp.Credentials = new NetworkCredential("testm3212@gmail.com", "test121416");
-                    smtp.EnableSsl = true;
-                    smtp.Send(mail);
-                }
-            }
-
-        }
+        
 
         public Admin()
         {
